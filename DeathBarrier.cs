93b7e3b32e49f4444169d14fd792fee496a03a7f
Wildan Yazid Ziddan <wildanziddan28@gmail.com>
2023-11-11 18:53:37 +0700

    first commit

---
+++ DeathBarrier.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DeathBarrier : MonoBehaviour
+{
+
+    public AudioSource dieSoundEffect;
+
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+    if (other.CompareTag("Player"))
+    {
+        other.gameObject.SetActive(false);
+        GameManager.Instance.ResetLevel(4f);
+        dieSoundEffect.Play();
+    }
+    else
+    {
+        Destroy(other.gameObject);
+    }
+   }
+}
---
+++ SideScrolling.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class SideScrolling : MonoBehaviour
+{
+   public Transform targetToFollow;
+
+   public float height = 6.5f;
+   public float undergroundHeight = -8.5f;
+
+   private void LateUpdate()
+   {
+     transform.position = new Vector3 (
+          Mathf.Clamp(targetToFollow.position.x, 15.5f, 88f),
+          transform.position.y,
+          transform.position.z);
+     
+   }
+
+   public void SetUnderground(bool underground)
+   {
+     Vector3 cameraPosition = transform.position;
+     cameraPosition.y = underground ? undergroundHeight : height;
+     transform.position = cameraPosition;
+   }
+}
+
+//    Vector3 cameraPosition = transform.position;
+     //    cameraPosition.x = player.position.x;
+     //    transform.position = cameraPosition;
+
+     //    Vector3 cameraPosition = transform.position;
+     //    cameraPosition.x = Mathf.Max(cameraPosition.x, player.position.x);
+     //    cameraPosition.x = Mathf.Clamp(cameraPosition.x, 1.5f, 200f);
+     //    transform.position = cameraPosition;
